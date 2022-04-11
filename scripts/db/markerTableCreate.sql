3e623954b17be2d771bf9a60cf73a5651212382d
Roy Njeru <roynjeru24@gmail.com>
2022-04-10 21:58:00 -0400

    Changed marker schema, V0 of scraping functions

--- scripts/db/markerTableCreate.sql
+++ scripts/db/markerTableCreate.sql
@@ -1,11 +1,21 @@
 use historee;
-create table markers (
+create table historee.markers (
     id int not null auto_increment,
+    hmdbID int,
     title varchar(80),
+    inscription varchar(750),
     latitude decimal,
     longitude decimal,
     town varchar(30),
     county varchar(30),
     state varchar(20),
-    primary key (id)
+    country varchar(40),
+    primary key (id),
+    unique(hmdbID)
 );
+
+-- sample insert query 
+insert into markers (title, latitude, longitude, town, county, state) values
+('Test locatioon', 33.8790, -84.2890, 'Savannah', 'Fulton', 'Georgia');
+
+-- truncate markers;
--- scripts/hmbd.py
+++ scripts/hmbd.py
@@ -2,27 +2,70 @@ from distutils.command.config import config
 import json
 import requests
 import os
+from xml.dom import minidom
 
 
 configFile = open(os.path.dirname(__file__)+ '/scriptsConfig.json')
 configData = json.load(configFile)
-
 userkey = "&userkey=" + configData['userkey']
-piedmontParkPoint = "&lat=33.7884801&Lon=-84.368617"
 
-# step 2: get data from api
-getUrl = "https://www.hmdb.org/m.asp?m=64905"
-nearbyUrl = "https://www.hmdb.org/db/getnearby.asp?range=5"
+def getInscription(aID):
+    markerUrl = "https://www.hmdb.org/db/getmarker.asp?markerid=" + str(aID) + userkey
+    getResponse = requests.get(markerUrl, headers={})
 
-getHeaders = {}
+    xmlText = getResponse.text
+    parser = minidom.parseString(xmlText)
+    hmdbXml = parser.childNodes[1].childNodes
 
-requestUrl = nearbyUrl + piedmontParkPoint + userkey
+    for i in range(0, hmdbXml.length):
+        if (hmdbXml[i].localName == "marker"):
+            marker = hmdbXml[i]
+            for j in range(0, marker.childNodes.length):
+                curAttr = marker.childNodes[j]
+                hmdbID = marker._attrs['markerid'].nodeValue
+                if(curAttr.localName == "inscription"):
+                    return(curAttr.firstChild.data)
 
-getResponse = requests.get(requestUrl, headers=getHeaders)
+def getNearbyPutDB(lat, long):
+    point = "&lat=" + str(lat) + "&Lon=" + str(long)
+    nearbyUrl = "https://www.hmdb.org/db/getnearby.asp?range=5"
 
-xmlDoc = getResponse.content
+    getHeaders = {}
 
-for i in range(0, 5):
-    title = ""
+    requestUrl = nearbyUrl + point + userkey
 
-print("Done")
+    getResponse = requests.get(requestUrl, headers=getHeaders)
+
+    xmlText = getResponse.text
+    parser = minidom.parseString(xmlText)
+    hmdbXml = parser.childNodes[1].childNodes
+
+    for i in range(0, hmdbXml.length):
+        if (hmdbXml[i].localName == "marker"):
+            marker = hmdbXml[i]
+            for j in range(0, marker.childNodes.length):
+                curAttr = marker.childNodes[j]
+                hmdbID = marker._attrs['markerid'].nodeValue
+                mInscription = getInscription(hmdbID)
+                if(curAttr.localName == "title"):
+                    mTitle = curAttr.firstChild.data
+                elif(curAttr.localName == "latitude"):
+                    mLatitue = curAttr.firstChild.data
+                elif(curAttr.localName == "longitude"):
+                    mLongitue = curAttr.firstChild.data
+                elif(curAttr.localName == "town"):
+                    mTown = curAttr.firstChild.data
+                elif(curAttr.localName == "county"):
+                    mCounty = curAttr.firstChild.data
+                elif(curAttr.localName == "state"):
+                    mState = curAttr.firstChild.data
+                elif(curAttr.localName == "country"):
+                    mCountry = curAttr.firstChild.data
+                
+            
+            print(mTitle, hmdbID, mInscription, mLatitue, mLongitue, mTown, mCounty, mState, mCountry)
+
+if __name__ == '__main__':
+    # Piedmont Park: 33.7884801,-84.368617
+    getNearbyPutDB(33.7884801, -84.368617)
+    # getInscription(17293)
